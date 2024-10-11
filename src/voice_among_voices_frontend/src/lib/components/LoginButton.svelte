670f332b0627b1ad9cd9342f608f25fc4ec8aa8e
Andri Schatz <andri.schatz@gmail.com>
2024-10-11 16:10:03 +0200

    try and implement connect2ic

---
+++ src/voice_among_voices_frontend/src/lib/components/LoginButton.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {ConnectButton, ConnectDialog, useConnect} from '@connect2ic/svelte';
+
+    const {isConnected, principal, activeProvider} = useConnect({
+        onConnect: () => {
+            console.log(isConnected, principal, activeProvider);
+        },
+        onDisconnect: () => {
+            // Signed out
+        },
+    });
+</script>
+
+<div>
+    <ConnectButton />
+    <ConnectDialog />
+</div>
--- src/voice_among_voices_frontend/src/routes/+layout.svelte
+++ src/voice_among_voices_frontend/src/routes/+layout.svelte
@@ -1,5 +1,33 @@
-<script>
+<script lang="ts">
     import '../app.css';
+
+    import {Connect2ICProvider} from '@connect2ic/svelte';
+
+    import {backend} from '$lib/canisters';
+
+    import {browser} from '$app/environment';
+    import {onMount} from 'svelte';
+
+    let client;
+
+    onMount(async () => {
+        if (browser) {
+            try {
+                const core = await import('@connect2ic/core');
+                const providers = await import('@connect2ic/core/providers');
+                client = core.createClient({
+                    canisters: {
+                        backend,
+                    },
+                    providers: providers.defaultProviders,
+                });
+            } catch (e) {
+                console.error(e);
+            }
+        }
+    });
 </script>
 
-<slot />
+<Connect2ICProvider>
+    <slot />
+</Connect2ICProvider>
