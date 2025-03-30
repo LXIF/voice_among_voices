3dde35a6e6022575f4a32319fc67ad73ffcbdcef
Andri Schatz <andri.schatz@dfinity.org>
2025-03-30 16:22:40 +0200

    implement ic-siwe-js

--- src/voice_among_voices_frontend/src/routes/+page.svelte
+++ src/voice_among_voices_frontend/src/routes/+page.svelte
@@ -3,13 +3,10 @@
     import Footer from "$lib/components/Footer.svelte";
     import NodeMapContainer from "$lib/components/NodeMapContainer.svelte";
     import AudioContainer from "$lib/components/AudioContainer.svelte";
-    import SiweContext from "$lib/siwe/SiweContext.svelte";
-    import { canisterId, idlFactory } from "../../../declarations/ic_siwe_provider";
 </script>
 
-<SiweContext {canisterId} {idlFactory}>
-        <Header />
-        <NodeMapContainer />
-        <AudioContainer />
-        <Footer />
-</SiweContext>
+
+<Header />
+<NodeMapContainer />
+<AudioContainer />
+<Footer />
