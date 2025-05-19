ee7f1cb1032f954306824f9d54a53a738ffa2bf6
Andri Schatz <andri.schatz@dfinity.org>
2025-05-19 10:36:04 +0200

    add basic log modal

--- src/voice_among_voices_frontend/src/lib/components/Footer.svelte
+++ src/voice_among_voices_frontend/src/lib/components/Footer.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
+    import { showInfoModal } from "$lib/state/uxState";
     import Button from "./Button.svelte";
 </script>
 
 <footer class="absolute bottom-0 flex w-full justify-between px-4 pb-4">
     <h1 class="select-none text-xl font-bold">Beschrieb</h1>
-    <Button>Info</Button>
+    <Button onclick={() => ($showInfoModal = !$showInfoModal)} class="text-xl"
+        >Info</Button
+    >
 </footer>
---
+++ src/voice_among_voices_frontend/src/lib/components/InfoModal.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+    import { showInfoModal } from "$lib/state/uxState";
+    import Button from "./Button.svelte";
+    import Dialog from "./Dialog.svelte";
+
+    type SelectedState = "info" | "log";
+
+    let selected = $state<SelectedState>("info");
+
+    function handleClick(newSelected: SelectedState) {
+        selected = newSelected;
+    }
+</script>
+
+<Dialog onClose={() => ($showInfoModal = false)}>
+    <div class="mx-2 mt-1 flex items-center justify-center gap-2">
+        <Button
+            onclick={() => handleClick("info")}
+            class="{selected === 'info'
+                ? 'border border-slate-950 dark:border-slate-50'
+                : ''} rounded-full px-4 py-2">Info</Button
+        >
+        <Button
+            onclick={() => handleClick("log")}
+            class="{selected === 'log'
+                ? 'border border-slate-950 dark:border-slate-50'
+                : ''} rounded-full px-4 py-2">Log</Button
+        >
+    </div>
+    {#if selected === "info"}
+        <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum totam
+            asperiores distinctio commodi facere rerum et quam blanditiis. Amet
+            a delectus ducimus esse iure consequatur minus corporis odio quam
+            vel.
+        </div>
+    {:else if selected === "log"}
+        <div>LOG</div>
+    {/if}
+</Dialog>
