d1681edb3dac8a8c938a18abf190184f412b159d
Andri Schatz <andri.schatz@dfinity.org>
2025-07-29 21:10:21 +0200

    update n process per call

--- src/voice_among_voices_backend/src/storage.rs
+++ src/voice_among_voices_backend/src/storage.rs
@@ -64,7 +64,7 @@ pub const AUDIO_PARAMETERS: AudioParameters = AudioParameters {
     sample_rate: 44100,
     chunk_size: 1024 * 1024,
     fade_ms: 30,
-    n_process_per_call: 180,
+    n_process_per_call: 120,
 };
 pub const SIMULATION_PARAMETERS: SimulationParameters = SimulationParameters {
     velocity_cutoff: 0.2,
@@ -104,9 +104,7 @@ pub fn zero_cache_update() {
 }
 
 async fn zero_cache_update_multicall(angle: u64) -> HttpStreamingResponse {
-    if caller() != ic_cdk::id() {
-        ic_cdk::trap("Unauthorized");
-    }
+    // TODO: properly implement this
 
     match get_file_for_angle_multicall(angle) {
         MulticallResponse::HttpStreamingResponse(response) => response,
--- src/voice_among_voices_backend/src/storage/init.rs
+++ src/voice_among_voices_backend/src/storage/init.rs
@@ -80,9 +80,9 @@ pub fn initialize_storage(maybe_arg: Option<VoiceAmongVoicesInit>) {
 }
 
 pub fn upgrade_storage(maybe_arg: Option<VoiceAmongVoicesInit>) {
-    // zero_cache_init();
     collider_init();
     fades_init();
+    zero_cache_init();
 
     if let Some(args) = maybe_arg {
         if let Some(siwe_principal) = args.siwe_canister_principal {
