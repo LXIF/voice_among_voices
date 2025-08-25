775008ca1bfe012562e7b4c5a2ab37e951fd664d
Andri Schatz <andri.schatz@dfinity.org>
2025-08-25 19:00:28 +0200

    update script

--- scripts/redeploy-token.sh
+++ scripts/redeploy-token.sh
@@ -1,9 +1,9 @@
 dfx deploy voice_among_voices_backend --ic --identity LXIF-private --argument $'(
     opt record {
         siwe_canister_principal = null;
-        token_address = opt "TOKEN";
+        token_address = opt "0xb32CEf004a828F0E2f87c6b188593f9cEd8FD01D";
         dev_mode = null;
         admin_token_id = null;
-        token_buy_link = opt "https://opensea.io/item/base/TOKEN/";
+        token_buy_link = opt "https://opensea.io/item/base/0xb32CEf004a828F0E2f87c6b188593f9cEd8FD01D/";
     }
 )'
