a55bf90a55283d1f2df74eb33a8fb0799e81793b
Andri Schatz <andri.schatz@dfinity.org>
2025-08-28 22:06:14 +0200

    spacing

--- src/voice_among_voices_frontend/src/routes/+page.svelte
+++ src/voice_among_voices_frontend/src/routes/+page.svelte
@@ -36,7 +36,9 @@
 </script>
 
 <Header />
-<div class="flex max-h-screen flex-col pt-[10vh] lg:min-h-screen lg:pt-0">
+<div
+    class="flex max-h-screen flex-col pt-[10vh] md:pt-[5vh] lg:min-h-screen lg:pt-0"
+>
     <div class="flex flex-1 items-center justify-center">
         <div class="flex flex-col items-center gap-4">
             <NodeMapContainer bind:this={nodeMapCont} />
